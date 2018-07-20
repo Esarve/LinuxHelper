9a80f7b56a68d64b84d4167315a36da2a213165d
Sourav <esarve.srv@gmail.com>
2018-07-20 14:40:25 +0600

    ADDED ripple effects

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
 public class Controller {
+    @FXML
+    private ImageView deb;
+
+    @FXML
+    private ImageView arch;
+
+
 }
