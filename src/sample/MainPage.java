8205625eb580325dc9a07a3c568a52a93e28800d
Sourav <esarve.srv@gmail.com>
2018-07-22 20:04:37 +0600

    Added browser (WIP)

--- src/sample/MainPage.java
+++ src/sample/MainPage.java
@@ -1,4 +1,18 @@
 package sample;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
 public class MainPage {
+
+    @FXML
+    JFXTextField locationPath;
+
+    @FXML
+    void openBrowser(MouseEvent event){
+
+    }
+
+
 }
