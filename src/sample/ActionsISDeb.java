32a4c1b43c2611b526cda7fca98222b0a67380f3
Sourav <esarve.srv@gmail.com>
2018-07-23 17:08:45 +0600

    Added class for installing softwares (WIP)

---
+++ src/sample/ActionsISDeb.java
@@ -0,0 +1,10 @@
+package sample;
+
+public class ActionsISDeb {
+
+    public String chrome = "chromium-browser";
+    public String htop= "htop";
+    public String firefox = "firefox";
+    public String thunderbird = "thunderbird";
+
+}
